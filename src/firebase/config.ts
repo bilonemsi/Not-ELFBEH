7f3c12806e11836ae27cc346251ac2e586ed075d
Behlül BİROL <behlulbirol@gmail.com>
2026-05-05 09:37:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6846512229-f93c9",
+  "appId": "1:594830853369:web:1e21dad4f3b70a92c5ce28",
+  "apiKey": "AIzaSyCwapuolDs4tiiRIhavp6Ewj20s43abXR0",
+  "authDomain": "studio-6846512229-f93c9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "594830853369"
+};
